e898dd2bf346585baceba8830b2aa3da3b709038
Shine <sabirovdima1@gmail.com>
2023-09-23 08:46:06 +0300

    added api

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { HTMLProps } from 'react'
 import styles from './page.module.scss'
 import ChatHeader from '@/components/chatHeader/chatHeader'
@@ -7,6 +5,8 @@ import ChatHistory from '@/components/chatHistory/chatHistory'
 import ChatFooter from '@/components/chatFooter/chatFooter'
 
 export default function Home(props: HTMLProps<HTMLDivElement>) {
+  const [messages, setMessages] = useState([]);
+
   return (
     <main className={(props.className ?? '') + ' ' + styles['wrapper']}>
       <ChatHeader/> 
--- front-end/components/chatFooter/chatFooter.tsx
+++ front-end/components/chatFooter/chatFooter.tsx
@@ -3,18 +3,33 @@
 import React, { HTMLProps, useEffect, useRef, useState } from 'react' 
 import styles from './chatFoote.module.scss'
 import { Input } from '../ui/input'
-import { Paperclip, Send, SendHorizontal } from 'lucide-react'
+import { Paperclip, Send } from 'lucide-react'
 
 export default function ChatFooter(props: HTMLProps<HTMLDivElement>) {
-    const [first, setfirst] = useState<number>(0)
-    const textRef = useRef(null)
-    
-  return (
+    const [isActive, setIsActive] = useState<boolean>(false);
+    const [isHover, setIsHover] = useState<boolean>(false);
+    const textRef = useRef<HTMLInputElement>(null);
+
+    const onChangeHandler = () => {
+        setIsActive(textRef.current?.value !== '');
+    } 
+    const onHoverHandler = () => {
+        setIsHover(!isHover);
+    }
+    const onClickHandler = () => {
+        if (textRef.current?.value === '') return;
+
+        
+    }
+
+    return (
     <div {...props} className={(props.className ?? '') + ' ' + styles['wrapper']}>
-        <Paperclip size={38} color='silver' className={styles['paperclip']}/>
-        <Input className={styles['input']} placeholder='Введите сообщение...'/>
-        {}
-        <Send size={38} color='silver' className={styles['send-horizontal']}/>
+        <Paperclip size={38} color={isHover ? 'red' : 'silver'} className={styles['paperclip']}
+        onMouseEnter={onHoverHandler} onMouseLeave={onHoverHandler}/> 
+        <Input className={styles['input']} placeholder='Введите сообщение...' 
+            ref={textRef} onChange={onChangeHandler}/> 
+        <Send size={38} color={isActive ? 'red' : 'silver'} onClick={onClickHandler}
+            className={isActive ? styles['send-horizontal'] : styles['send']}/>
     </div>
   )
 }
